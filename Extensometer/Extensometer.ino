cfd537e4fd48dd83661cc70e4a8858be346db9df
Zyphit <zyphit@users.noreply.github.com>
2018-03-02 15:03:51 -0800

    Add files via upload
    Initial upload of in progress code.

---
+++ Extensometer/Extensometer.ino
@@ -0,0 +1,130 @@
+/*
+  SD card datalogger
+
+ This example shows how to log data from three analog sensors
+ to an SD card using the SD library.
+
+ The circuit:
+ * analog sensors on analog ins 0, 1, and 2
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+
+ created  24 Nov 2010
+ modified 9 Apr 2012
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ */
+
+#include <SPI.h>
+#include <SD.h>
+#include <NewPing.h>
+#include <Wire.h>
+#include "RTClib.h"
+
+#define TRIGGER_PIN  5  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     6  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+RTC_DS1307 rtc;
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+const int chipSelect = 10;
+
+float startime;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
+  Serial.println("Marking start time:");
+  timestamp();
+ 
+}
+
+void loop() {
+  DateTime now = rtc.now();
+
+  String dataString = "";
+
+  int sensor = sonar.ping_cm();
+  dataString += String(sensor);
+
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  if (dataFile) {
+    if (!sensor == 0){
+      dataFile.println(dataString);
+      Serial.println(dataString);
+    }  
+    dataFile.close();
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+ delay(300);
+}
+
+void timestamp() {
+  DateTime now = rtc.now;
+ Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    String timenow = "";
+    timenow = now.year();
+    timenow += " ";
+    timenow += now.month();
+    timenow += " ";
+    timenow += now.day();
+    timenow += " ";
+    timenow += now.hour();
+    timenow += " ";
+    timenow += now.minute();
+    timenow += " ";
+    timenow += now.second();
+    File dataFile = SD.open("datalog.txt", FILE_WRITE);
+    dataFile.println(timenow);
+    Serial.println(timenow);
+    dataFile.close();
+}
+
+
+
+
+
+
